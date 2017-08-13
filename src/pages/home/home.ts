5a41ad3c14475367056076fafeca3b4bae435c47
leahlang4d <leahlang4d@gmail.com>
2017-08-13 12:02:12 -0700

    updated pcs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {LoadingController, NavController} from 'ionic-angular';
 import {Http} from "@angular/http";
 import 'rxjs/add/operator/map';
-
+import {InAppBrowser} from 'ionic-native';
 
 
 @Component({
@@ -12,7 +12,7 @@ import 'rxjs/add/operator/map';
 export class HomePage {
 
   public feeds: Array <string>;
-  private url: string = "https://www.reddit.com/new.json" ;
+  private url: string = "https://www.reddit.com/r/Futurology/new.json" ;
 
   constructor(public navCtrl: NavController, public http: Http, public loadingCtrl:LoadingController) {
     this.fetchContent();
@@ -25,12 +25,27 @@ export class HomePage {
 
     loading.present();
 
-    this .http.get (this .url) .map (res => res.json())
+    this.http.get (this.url).map (res => res.json())
       .subscribe ( data => {
-          this .feeds = data.data.children;
+          this.feeds = data.data.children;
+
+          this.feeds.forEach((e, i, a) => {
+          if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1 ) {
+            e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
+          }
+        })
           loading.dismiss();
       });
 
+
+
+  }
+
+  itemSelected (url: string): void{
+    let browser = new InAppBrowser (url, '_system');
   }
 
 }
+
+//TODO
+//figure out how to get links to work for mobile
